f5ec0470c2e56524cd2de90628752c67eb79c3b8
Milzam Radifan <milzam_radifan@student.ub.ac.id>
2024-03-12 04:31:34 +0700

    [add] search activity layout and bottom nav bar logic.

--- app/src/main/java/com/example/tapetrove/MainActivity.java
+++ app/src/main/java/com/example/tapetrove/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.tapetrove;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_home) {
+                // Handle Home item click
+                return true;
+            } else if (item.getItemId() == R.id.bottom_search) {
+                // Handle Search item click
+                startActivity(new Intent(getApplicationContext(), search.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_profile) {
+                // Handle Profile item click
+//                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                finish();
+                return true;
+            }
+            return false;
         });
     }
 }
--- app/src/main/java/com/example/tapetrove/search.java
+++ app/src/main/java/com/example/tapetrove/search.java
@@ -1,5 +1,6 @@
 package com.example.tapetrove;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class search extends AppCompatActivity {
 
@@ -20,5 +23,26 @@ public class search extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_search);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_home) {
+                // Handle Home item click
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_search) {
+                // Handle Search item click
+                return true;
+            } else if (item.getItemId() == R.id.bottom_profile) {
+                // Handle Profile item click
+//                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                finish();
+                return true;
+            }
+            return false;
+        });
+
     }
 }
