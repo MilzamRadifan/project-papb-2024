acf2718a74692b34249c3c933d8fbcd9ab152fa2
Muhammad Fadhiil <fadhilrmdhn3010@gmail.com>
2024-03-12 00:23:43 +0700

    [add] Add RatingActivity etc

--- app/src/main/java/com/example/tapetrove/MainActivity.java
+++ app/src/main/java/com/example/tapetrove/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tapetrove;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Mendapatkan referensi ke tombol
+        Button button = findViewById(R.id.button);
+
+        // Menambahkan pendengar klik ke tombol
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Membuat Intent untuk memulai RatingActivity
+                Intent intent = new Intent(MainActivity.this, RatingActivity.class);
+                startActivity(intent); // Memulai aktivitas RatingActivity
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/tapetrove/RatingActivity.java
@@ -0,0 +1,61 @@
+package com.example.tapetrove;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RatingActivity extends AppCompatActivity {
+    RatingBar rb_ratingBar;
+    float myRating = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_rating);
+
+        rb_ratingBar = (RatingBar) findViewById(R.id.rb_ratingBar);
+        rb_ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+                int rating = (int) v;
+                String message = null;
+
+                myRating = rb_ratingBar.getRating();
+                switch (rating) {
+                    case 1:
+                        message = "Saya Kecewa!";
+                        break;
+                    case 2:
+                        message = "Saya Sedih!";
+                        break;
+                    case 3:
+                        message = "Biasa Saja!";
+                        break;
+                    case 4:
+                        message = "Saya Puas!";
+                        break;
+                    case 5:
+                        message = "Saya Sangat Puas!";
+                        break;
+                }
+
+                Toast.makeText(RatingActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
