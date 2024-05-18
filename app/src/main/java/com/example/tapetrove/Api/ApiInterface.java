04d78ab04824115747dc9d91e40771a3ac6d1838
Milzam Radifan <milzam_radifan@student.ub.ac.id>
2024-05-18 14:23:21 +0700

    [add] init project

---
+++ app/src/main/java/com/example/tapetrove/Api/ApiInterface.java
@@ -0,0 +1,35 @@
+package com.example.tapetrove.Api;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+//
+  @GET("/3/discover/movie")
+  Call<ResponseBody> sortMovies (
+          @Query("api_key") String apiKey,
+          @Query("language") String language,
+          @Query("page") int page,
+          @Query("sort_by") String sort
+  );
+
+  @GET("/3/search/movie")
+  Call<ResponseBody> searchMovie (
+          @Query("api_key") String apiKey,
+          @Query("query") String identifier,
+          @Query("language") String language,
+          @Query("page") int page
+
+  );
+
+  @GET("/3/movie/{category}")
+  Call<ResponseBody> listOfMovies(
+          @Path("category") String category,
+          @Query("api_key") String apiKey,
+          @Query("language") String language,
+          @Query("page") int page
+  );
+}
