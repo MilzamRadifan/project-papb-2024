2cfaa0298a431a412f8fcf72a10f17920ed8bac1
Muhammad Fadhiil <fadhilrmdhn3010@gmail.com>
2024-06-08 16:37:20 +0700

    [fix] Fix update profile and display name in ProfileFragment

--- app/src/main/java/com/example/tapetrove/Activity/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/tapetrove/Activity/Profile/EditProfileFragment.java
@@ -1,110 +1,100 @@
 package com.example.tapetrove.Activity.Profile;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.room.Room;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.tapetrove.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditProfileFragment extends Fragment {
+    EditText etUsername, etnomorhp, etAlamat;
+    Button btSimpan;
+    DatabaseReference databaseReference;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private EditText etEmail, etUsername, etNoTelpon, etAlamat;
-//    private DatabaseReference mDatabase;
-//    private FirebaseAuth mAuth;
-
-    public EditProfileFragment() {
-        // Required empty public constructor
-    }
-
-    public static EditProfileFragment newInstance(String username, String email, String noTelpon, String alamat) {
-        EditProfileFragment fragment = new EditProfileFragment();
-        Bundle args = new Bundle();
-        args.putString("username", username);
-        args.putString("email", email);
-        args.putString("noTelpon", noTelpon);
-        args.putString("alamat", alamat);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public EditProfileFragment(){}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
         etUsername = view.findViewById(R.id.etUsername);
-        etEmail = view.findViewById(R.id.etEmail);
-        etNoTelpon = view.findViewById(R.id.etnomorhp);
+        etnomorhp = view.findViewById(R.id.etnomorhp);
         etAlamat = view.findViewById(R.id.etAlamat);
-        Button btSimpan = view.findViewById(R.id.btSimpan);
+        btSimpan = view.findViewById(R.id.btSimpan);
 
-//        mAuth = FirebaseAuth.getInstance();
-//        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String username = snapshot.child("username").getValue(String.class);
+                    String noHP = snapshot.child("telephone").getValue(String.class);
+                    String alamat = snapshot.child("address").getValue(String.class);
 
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            String username = bundle.getString("username");
-            String email = bundle.getString("email");
-            String noTelpon = bundle.getString("noTelpon");
-            String alamat = bundle.getString("alamat");
+                    etUsername.setText(username);
+                    etnomorhp.setText(noHP);
+                    etAlamat.setText(alamat);
+                }
+            }
 
-            etUsername.setText(username);
-            etEmail.setText(email);
-            etNoTelpon.setText(noTelpon);
-            etAlamat.setText(alamat);
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
         btSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newUsername = etUsername.getText().toString();
-                String newEmail = etEmail.getText().toString();
-                String newNoTelpon = etNoTelpon.getText().toString();
-                String newAlamat = etAlamat.getText().toString();
-
-//                DatabaseReference userRef = mDatabase.child(mAuth.getCurrentUser().getUid());
-
-//                userRef.child("username").setValue(newUsername);
-//                userRef.child("email").setValue(newEmail);
-//                userRef.child("telephone").setValue(newNoTelpon);
-//                userRef.child("address").setValue(newAlamat);
-
-                Bundle result = new Bundle();
-                result.putString("newusername", newUsername);
-                result.putString("newemail", newEmail);
-                result.putString("newnotelpon", newNoTelpon);
-                result.putString("newalamat", newAlamat);
-                getParentFragmentManager().setFragmentResult("editProfileResult", result);
-
-                // Setelah menyimpan, kembali ke ProfileFragment
-                requireActivity().getSupportFragmentManager().popBackStack();
+                updateUser();
             }
         });
 
+        return view;
+    };
 
+    private void updateUser(){
+        String username = String.valueOf(etUsername.getText());
+        String noHP = String.valueOf(etnomorhp.getText());
+        String alamat = String.valueOf(etAlamat.getText());
+
+        Map<String, Object> updateData = new HashMap<>();
+        updateData.put("username", username);
+        updateData.put("telephone", noHP);
+        updateData.put("address", alamat);
+
+        databaseReference.updateChildren(updateData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(getActivity(), "Profile has been changed", Toast.LENGTH_SHORT).show();
+                        requireActivity().getSupportFragmentManager().popBackStack();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "Profile changed failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/tapetrove/Activity/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/tapetrove/Activity/Profile/ProfileFragment.java
@@ -5,24 +5,63 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.tapetrove.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
     private Button btEdit, btOut;
     private LinearLayout menuRent, menuWishlist, menuEditProfile, menuDelAcc, menuSignOut;
+    TextView tvUsername, tvEmail;
+    DatabaseReference useRef;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-// Inisialisasi menu
+
+        // Menampilkan Informasi Akun
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvEmail = view.findViewById(R.id.tvEmail);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            String email = user.getEmail();
+            tvEmail.setText(email);
+
+            useRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+            useRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()){
+                        String username = snapshot.child("username").getValue(String.class);
+                        tvUsername.setText(username);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    tvUsername.setText("Unknown");
+                }
+            });
+        }
+
+        // Inisialisasi menu
         menuRent = view.findViewById(R.id.menuRent);
         menuWishlist = view.findViewById(R.id.menuWishlist);
         menuEditProfile = view.findViewById(R.id.menuEditProfile);
